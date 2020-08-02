8ad2beb3f1a07344b211b52a98a407d5589865e4
MrcSnm <msnmancini@hotmail.com>
2020-08-02 15:44:44 -0300

    Making build system

--- README.md
+++ README.md
@@ -11,8 +11,65 @@ As the purpose of this template is to be beginner friendly, I will try to mantai
 
 ## Important notes
 This project was made for working on Linux, so, it is untested on Windows, but it should not differ too much
-
+> ldc2 is a command on your console
+> You should have modified (dlangPath)/(ldc)/etc/ldc2.conf to include your target architectures
+> You should have defined on your environment the variable "ANDROID_NDK_HOME"
 
 ## Building
 The build system will be a bit different of the main build system for D, which is called `dub`. The reasoning is for simply
 keeping your build with some library filtering(for the target Android architectures)
+
+### Modifying build.d
+If you need to modify the main build system, it is quite simple to add your own things to it
+There are arrays definitions for each thing:
+- Architectures to output, default is:
+```
+[
+    "aarch64",
+    "armv7a",
+    "x86_64",
+    "i686"
+];
+```
+- Sources for compile, default is:
+```
+[
+    "source/",
+	"bindbc-loader/source",
+	"bindbc-sdl/source"
+];
+```
+- Arch Folders
+- Dependencies, default is:
+```
+[
+	"BindBC-Loader", 
+	"BindBC-SDL"
+];
+```
+- Versions for the compiler, default is:
+```
+[
+	"SDL_2012",
+	"BindSDL_Mixer", 
+	"BindSDL_TTF", 
+	"BindSDL_Image"
+];
+```
+- Debug, default is: `[]`
+- Libraries, those are what libraries will be imported from android ndk, default is:
+```
+[
+    "log"
+];
+```
+- TripleSystem, this is where you will need to change if you're working on Windows on Mac, default is:
+`enum tripleSystem = "-linux-android";`
+
+## Acknowledgements
+
+- LDC team for making the compiler, this project would not be anything without them
+- https://www.libsdl.org/ for providing SDL
+- @mdparker for providing BindBC-SDL and helping me on things about compiling
+- @Joakim for making the initial jni.d  lib
+- @Vild for converting my script into a *.d file
--- build.d
+++ build.d
@@ -1,24 +1,76 @@
 #!/usr/bin/rdmd
 import std;
-
+/**
+*	This will probably not change any soon
+**/
 string compiler = "ldc2";
-string[] archs = ["aarch64","armv7a","x86_64","i686"];
-
-string[] sourcePaths = [
+/**
+*	If you need to add a new architecture to output
+**/
+string[] archs = 
+[
+	"aarch64",
+	"armv7a",
+	"x86_64",
+	"i686"
+];
+string[string] archFolders;
+archFolders["aarch64"] = "arm64-v8a";
+archFolders["armv7a"] = "armeabi-v7a";
+archFolders["x86_64"] = "x86_64";
+archFolders["i686"] = "x86";
+/**
+*	If you need to add a new source
+**/
+string[] sourcePaths = 
+[
 	"source/",
-	// These should be local git submodules or something instead of getting them from the ~/.dub/packages
+	"jni/",
 	"bindbc-loader/source",
 	"bindbc-sdl/source"
 ];
-string[] dependencies = ["BindBC-Loader", "BindBC-SDL"];
-string[] versions = ["SDL_2012", "BindSDL_Mixer", "BindSDL_TTF", "BindSDL_Image"];
-string[] debugs = [];
-string[] libraries = ["log"];
+string[] dependencies = 
+[
+	"BindBC-Loader", 
+	"BindBC-SDL"
+];
+/**
+*	Versions for the ldc compiler
+**/
+string[] versions = 
+[
+	"SDL_2012",
+	"BindSDL_Mixer", 
+	"BindSDL_TTF", 
+	"BindSDL_Image"
+];
+/**
+*	Debug options
+**/
+string[] debugs = 
+[
 
+];
+
+/**
+*	Android libraries taken from NDK
+**/
+string[] libraries = 
+[
+	"log"
+];
+
+/**
+*	NDK Api Level, the first number(major) on the Android/Sdk/ndk/(major.minor)
+**/
 enum ndkApiLevel = 21;
+/**
+*	If you're compiling it from windows or Mac, you will need to change this tripleSystem
+**/
 enum tripleSystem = "-linux-android";
 
-string[] getSources(string path) {
+string[] getSources(string path) 
+{
 	string[] files;
 	foreach (DirEntry e; path.dirEntries(SpanMode.depth).filter!(f => f.name.endsWith(".d")))
 		files ~= e.name;
@@ -36,22 +88,37 @@ void buildProgram(string arch, string[] sources) {
 	foreach (debug_; debugs)
 		command ~= format!"-d-debug=%s"(debug_);
 	foreach (library; libraries)
-		command ~= format!"-l%s"(library);
+		command ~= format!"-L=-l%s"(library); //-L= for using GNU Linker(ld)
 
 	command ~= format!"-mtriple=%s-%s"(arch, tripleSystem);
 	command ~= "--shared";
-	command ~= "--of=libmain.so";
+	command ~= format!"--of=%s/libmain.so"(archFolders[arch]);
 
 	string androidLibs = format!"%s/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/%s%s/%s/"(environment["ANDROID_NDK_HOME"], arch, tripleSystem, ndkApiLevel);
-	command ~= format!"-L%s"(androidLibs);
+	command ~= format!"-L=-L%s"(androidLibs)~" ";
 
-	Pid pid = spawnProcess("/bin/echo" ~ command);
+	foreach(source; sources)
+	{
+		command~= source;
+	}
+
+	// Pid pid = spawnProcess("/bin/echo" ~ command);
+	Pid pid = spawnProcess(command);
 	pid.wait;
 }
 
-void main() {
+void main(string[] args) 
+{
 	// For debuging
-	environment["ANDROID_NDK_HOME"] = "derp";
+	try
+	{
+		environment["ANDROID_NDK_HOME"];
+	}
+	catch(Exception e)
+	{
+		throw new Error("You must define on your environment the variable ANDROID_NDK_HOME");
+	}
+	writeln("Getting NDK from: " ~ environment["ANDROID_NDK_HOME"]~"/");
 
 	string[] sources;
 	foreach (sourcePath; sourcePaths)
